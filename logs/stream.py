be4dc91b8f5e3cc93f520f3dd1d46e2e3e8910cc
zippytarun1 <tarun.kakkar@snapdeal.com>
2017-03-22 18:06:10 +0530

    Update stream.py

--- logs/stream.py
+++ logs/stream.py
@@ -4,6 +4,8 @@ import time
 import eventlet
 eventlet.monkey_patch()
 
+#this is done to spawn the request
+
 app = flask.Flask(__name__)
 
 def follow(thefile):
@@ -14,7 +16,7 @@ def follow(thefile):
             time.sleep(0.1)
             continue
         yield line
-
+#reads function will follow the latest stream
 def reads():
     logfile = open("D:\logs\output.txt","r")
     loglines = follow(logfile)
@@ -25,11 +27,14 @@ def reads():
 @app.route( '/stream' )
 def stream():
     return flask.Response(reads(), mimetype= 'text/event-stream' )
-
+#this function will return data in the form of stream
 
 @app.route('/page')
 def get_page():
     return flask.send_file('page.html')
 
+#Streamed data will appear in 127.0.0.1:8080/page
+
+#application will run at 8088 and processes will tell you how many request this webservice can handle at one point of time
 if __name__ == "__main__":
     app.run(port=8088,processes=10)
